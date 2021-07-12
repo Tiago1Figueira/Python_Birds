fe639436660bca202a6c124d3ea6529643f1515b
tiago1figueira <tiago_figueira@hotmail.com>
2021-07-11 21:50:30 -0300

    Deletei ramo1 gerador de conflito.

--- orientacao_objetos/test_carro.py
+++ orientacao_objetos/test_carro.py
@@ -1,3 +1,9 @@
+from unittest import TestCase
 
 
-class CarroTestCase(TestC):
+class CarroTestCase(TestCase)
+    def teste_velocidade_inicial(self):
+        motor = Motor()
+        self.assertEqual()
+
+
