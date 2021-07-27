8851dc7d90951df940fb9c3c5de9200b38b437b5
tiago <tiago_figueira@hotmail.com>
2021-07-27 15:40:18 -0300

    Continuação do código pra testar o módulo carro.py usando o Unittest ao invés do doctest.

--- orientacao_objetos/teste_carro.py
+++ orientacao_objetos/teste_carro.py
@@ -1,3 +1,4 @@
+"""
 from unittest import TestCase
 
 from orientacao_objetos.carro import Motor
@@ -6,6 +7,23 @@ class CarroTestCase(TestCase):
     def teste_velocidade_inicial(self):
         motor = Motor()
         self.assertEqual(0, motor.velocidade)
+"""
+from unittest import TestCase
+
+from orientacao_objetos.carro import Motor
+
+
+class CarroTestCase(TestCase):
+    def teste_velocidade_inicial(self):
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
+
+
+
 
 
 
